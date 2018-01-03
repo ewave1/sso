56d7bed31ea5a944ebc186feed90fc05c7d95138
ewave <ewave11@hotmail.com>
2018-01-03 19:02:32 +0800

    init

---
+++ SmartSSO/Models/HomeLoginRequest.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartSSO.Models
+{
+    public class HomeLoginRequest
+    {
+        public HomeLoginRequest()
+        {
+            UserName = string.Empty;
+            Password = string.Empty;
+        }
+
+        [StringLength(32)]
+        [Required] 
+        public string UserName { get; set; }
+
+        [StringLength(32)]
+        [Required]
+        [Display(Name = "登录密码")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public void Trim()
+        {
+            if (!string.IsNullOrEmpty(UserName)) UserName = UserName.Trim();
+
+            if (!string.IsNullOrEmpty(Password)) Password = Password.Trim();
+
+        }
+    }
+}
