aa4adf9f82188a28da9526ca157c14f7cb464e07
ewave <ewave11@hotmail.com>
2018-01-04 11:32:16 +0800

    sso

---
+++ Data/Entities/InquiryLog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    /// <summary>
+    /// 报价记录
+    /// </summary>
+    public class InquiryLog
+    {
+        //输入
+        /// <summary>
+        /// 自增
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 编码
+        /// </summary>
+        [Required]
+        [MaxLength(100)]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 内径
+        /// </summary>
+        [Required]
+        public decimal SizeA { get; set; }
+
+        /// <summary>
+        /// 线径
+        /// </summary>
+        [Required]
+        public decimal SizeB { get; set; }
+
+
+        public string Material { get; set; }
+        public int MaterialId { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        [Required]
+        public DateTime CreateTime { get; set; }
+        //输出
+
+        //时间
+    }
+}
